ded66818ee274de4fed52a1a81f514c0ed01724d
이승훈 <95062919+leesh1997@users.noreply.github.com>
2023-11-15 16:54:05 +0900

    get_next_line_utils.c
    get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seunghun <seunghun@student.42.kr>          +#+  +:+       +#+        */
+/*   By: seunghun <seunghun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 17:09:30 by seunghun          #+#    #+#             */
-/*   Updated: 2023/10/28 15:27:53 by seunghun         ###   ########.fr       */
+/*   Updated: 2023/11/15 16:45:27 by seunghun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(char const *str)
+size_t	ft_strlen(char *str)
 {
 	size_t	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 	{
 		i++;
@@ -24,17 +26,19 @@ size_t	ft_strlen(char const *str)
 	return (i);
 }
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(char *src, char **save)
 {
 	char	*str;
 	int		i;
 	int		len;
 
 	i = 0;
+	if (!src)
+		return (free_str(save));
 	len = ft_strlen(src);
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
-		return (0);
+		return (free_str(save));
 	while (i < len)
 	{
 		str[i] = src[i];
@@ -44,7 +48,7 @@ char	*ft_strdup(char *src)
 	return (str);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char *s, unsigned int start, char **save, size_t len)
 {
 	char	*dest;
 	size_t	s_len;
@@ -59,21 +63,25 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	else
 		mal_len = len;
 	if (s_len < start)
-		return (ft_strdup(""));
+		return (ft_strdup("", save));
 	dest = (char *)malloc(sizeof(char) * (mal_len + 1));
 	if (!dest)
-		return (0);
-	i = 0;
-	while (i < len && i < s_len - start)
-	{
+		return (free_str(save));
+	i = -1;
+	while (++i < len && i < s_len - start)
 		dest[i] = s[start + i];
-		i++;
-	}
 	dest[i] = '\0';
 	return (dest);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*free_str(char **str)
+{
+	free(*str);
+	*str = 0;
+	return (0);
+}
+
+char	*ft_strjoin(char **s1, char **s2, char **save)
 {
 	char	*dest;
 	size_t	s1_len;
@@ -81,40 +89,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 	size_t	j;
 
-	i = 0;
+	i = -1;
 	j = 0;
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	if (!s1 && !s2)
-		return (0);
+	s1_len = ft_strlen(*s1);
+	s2_len = ft_strlen(*s2);
 	dest = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!dest)
-		return (0);
-	while (s1[i])
 	{
-		dest[i] = s1[i];
-		i++;
+		free_str(save);
+		return (0);
 	}
-	while (s2[j])
-		dest[i++] = s2[j++];
+	while ((*s1)[++i])
+		dest[i] = (*s1)[i];
+	while ((*s2)[j])
+		dest[i++] = (*s2)[j++];
 	dest[i] = '\0';
 	return (dest);
-}
-
-char	*ft_strchr(const char *str, int c)
-{
-	char	chg_c;
-	int		i;
-
-	i = 0;
-	chg_c = (unsigned char)c;
-	while (str[i])
-	{
-		if (str[i] == chg_c)
-			return ((char *)&str[i]);
-		i++;
-	}
-	if (str[i] == chg_c)
-		return ((char *)&str[i]);
-	return (0);
 }
